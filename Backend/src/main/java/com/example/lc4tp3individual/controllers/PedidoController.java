ae7e59b0290931f4f605bd0169e2dfef20affee8
Vata74 <vatatamola@gmail.com>
2024-05-18 23:00:23 -0300

    Arreglo funcionalidad Pedido

--- Backend/src/main/java/com/example/lc4tp3individual/controllers/PedidoController.java
+++ Backend/src/main/java/com/example/lc4tp3individual/controllers/PedidoController.java
@@ -1,26 +1,13 @@
 package com.example.lc4tp3individual.controllers;
 
 import com.example.lc4tp3individual.entities.Pedido;
-import com.example.lc4tp3individual.entities.PedidoDetalle;
-import com.example.lc4tp3individual.services.PedidoService;
 import com.example.lc4tp3individual.services.PedidoServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDate;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/pedido")
-public class PedidoController {
-
-    @Autowired
-    private PedidoService pedidoService;
-
-    @PostMapping("/create")
-    public ResponseEntity<Pedido> createPedido(@RequestBody Pedido pedido) {
-        pedido.setFechaPedido(LocalDate.now());
-        Pedido savedPedido = pedidoService.savePedido(pedido);
-        return ResponseEntity.ok(savedPedido);
-    }
+@CrossOrigin(origins = "*")
+@RequestMapping(path = "/pedido")
+public class PedidoController extends BaseControllerImp<Pedido, PedidoServiceImpl>{
 }
